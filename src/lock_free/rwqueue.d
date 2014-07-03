c3f1bf3b4e3476aa35d810115b4bd3446380643d
Martin Nowak <martin.nowak@plugintheworld.com>
2014-07-03 19:00:45 +0200

    add single-reader, single-writer queue

---
+++ src/lock_free/rwqueue.d
@@ -0,0 +1,132 @@
+module lock_free.rwqueue;
+
+import core.atomic;
+
+/**
+ * A lock-free single-reader, single-writer FIFO queue.
+ */
+shared struct RWQueue(T, size_t capacity = roundPow2!(PAGE_SIZE / T.sizeof))
+{
+    static assert(capacity > 0, "Cannot have a capacity of 0.");
+    static assert(roundPow2!capacity == capacity, "The capacity must be a power of 2");
+
+    @property size_t length() shared const
+    {
+        return wpos - rpos;
+    }
+
+    @property bool empty() shared const
+    {
+        return !length;
+    }
+
+    @property bool full() const
+    {
+        return length == capacity;
+    }
+
+    void push(shared(T) t)
+    in { assert(!full); }
+    body
+    {
+        _data[wpos & mask] = t;
+        incw();
+    }
+
+    shared(T) pop()
+    in { assert(!empty); }
+    body
+    {
+        auto res = _data[rpos & mask];
+        incr();
+        return res;
+    }
+
+private:
+    //    import std.algorithm; // move
+
+    enum mask = capacity - 1;
+
+    @property size_t wpos() shared const
+    {
+        return atomicLoad!(MemoryOrder.raw)(this._wpos);
+    }
+
+    @property size_t rpos() const
+    {
+        return atomicLoad!(MemoryOrder.raw)(this._rpos);
+    }
+
+    void incw()
+    {
+        atomicStore!(MemoryOrder.raw)(_wpos, wpos + 1);
+    }
+
+    void incr()
+    {
+        atomicStore!(MemoryOrder.raw)(_rpos, rpos + 1);
+    }
+
+    size_t _wpos;
+    size_t _rpos;
+    T[capacity] _data;
+}
+
+private:
+
+enum PAGE_SIZE = 4096;
+
+template roundPow2(size_t v)
+{
+    import core.bitop : bsr;
+    enum roundPow2 = v ? cast(size_t)1 << bsr(v) : 0;
+}
+
+static assert(roundPow2!0 == 0);
+static assert(roundPow2!3 == 2);
+static assert(roundPow2!4 == 4);
+
+version (unittest)
+{
+    import core.thread, std.concurrency;
+    enum amount = 10_000;
+
+    void push(T)(ref shared(RWQueue!T) queue)
+    {
+        foreach (i; 0 .. amount)
+        {
+            while (queue.full)
+                Thread.yield();
+            queue.push(cast(shared T)i);
+        }
+    }
+
+    void pop(T)(ref shared(RWQueue!T) queue)
+    {
+        foreach (i; 0 .. amount)
+        {
+            while (queue.empty)
+                Thread.yield();
+            assert(queue.pop() == cast(shared T)i);
+        }
+    }
+}
+
+unittest
+{
+    shared(RWQueue!size_t) queue;
+    auto t0 = new Thread({push(queue);}),
+        t1 = new Thread({pop(queue);});
+    t0.start(); t1.start();
+    t0.join(); t1.join();
+}
+
+unittest
+{
+    static struct Data { size_t i; }
+    shared(RWQueue!Data) queue;
+    auto t0 = new Thread({push(queue);}),
+        t1 = new Thread({pop(queue);});
+    t0.start(); t1.start();
+    t0.join(); t1.join();
+}
